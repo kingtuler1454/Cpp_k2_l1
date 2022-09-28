7e008c149e9bbcd9150caf06f098fa4870c0109b
kingtuler1454 <morozovsergey.03@gmail.com>
2022-09-28 16:19:58 +0400

    base

--- main.cpp
+++ main.cpp
@@ -1,23 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
 class Polynomial{
+public:
     struct Node {
 	int coefficent;
     int degree;
 	struct Node *next;
     };
-    Polynomial( int start_degree){
+
+
+    Polynomial( int start_degree,int start_coefficent){
     Node *head = NULL;
-    Node *tmp=(Node*)malloc(sizeof(Node)); # создаем узел
-    tmp->coefficent=1;
+    Node *tmp=(Node*)malloc(sizeof(Node)); // создаем узел
     tmp->degree=start_degree;
-    tmp->next=(*head)
-    (*head)=tmp;
+    tmp->next =head;
+    head = tmp;
+    tmp->coefficent=start_coefficent;
     }
-    int operator [](int & x){
-        printf('%d', x.coefficent);    
+
+    void operator [](int  x)
+    {   
+        cout<<x; 
+    }
+
+    
+    void set(int coefficent, int degree )
+    {
+    Node *tmp=(Node*)malloc(sizeof(Node)); // создаем узел
+    tmp->degree=degree;
+    //tmp->next =head;
+    //head = tmp;
+    tmp->coefficent=coefficent;
     }
 };
 int main(){
-
-    Polynomial A(5);
+    int degree, coefficent;
+    do:
+        cout<<"Введите степень многочлена: ";
+        cin>> degree;
+        cout<<"Выберите  коэффицент для  степени "<<degree;
+        cin>> coefficent;
+        cout<<"Будем ли вводить еще коеффицет? ";
+        cin>>
+    Polynomial A(degree, coefficent);
+    while()
     A[5];
+    //printf("%d", 5);
 }
